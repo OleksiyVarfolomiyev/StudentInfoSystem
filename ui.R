ddfd9f15752c3d8b393217638bccb96a8433c7b6
OleksiyVarfolomiyev <av_business7@yahoo.com>
2015-11-02 15:54:01 -0500

    Final Grade Modeler Added

--- ui.R
+++ ui.R
@@ -5,54 +5,48 @@ shinyUI(
  fluidPage(
   pageWithSidebar(
     
-    headerPanel("MATH 111-105"),
+    headerPanel("Student Information System MATH 111-105"),
 
-    wellPanel(
-      textInput('studentID', 'Enter Student ID (8-digit)')
+#    wellPanel(
+#      textInput('studentID', 'Enter Student ID (8-digit)')
      # submitButton('Login')
-    ),
+#    ),
     
-    
-#    mainPanel(
-  
-    conditionalPanel("input.studentID >10000000",
-      #wellPanel(
-        sidebarPanel(
-        h2('Grade Modeler'),
-        h4('Use sliders to model the Projected Final Grade'),
-        
-                       sliderInput("sliderAssignmentsAve", label = h3("Projected Assignments Ave Grade"), 
-                                   min = 0, max = 7, value = 4, step = 1),
-                       
-                       sliderInput("sliderExam3", label = h3("Projected Exam 3 Grade"), 
-                                   min = 0, max = 100, value = 65),
-                       
-                       sliderInput("sliderFinal", label = h3("Projected Final Exam Grade"), 
-                                   min = 0, max = 100, value = 65),
-                       h2('Projected Final Grade'),
-                       textOutput('projectedGrade'),
-                       h2('Projected Final Letter Grade'),
-                       textOutput('projectedLetterGrade'),
-      #),
-      
-      #wellPanel(               
+#conditionalPanel("input.studentID >10000000| input.studentID == 0",
+     
+      sidebarPanel(
+        textInput('studentID', 'Enter Student ID (8-digit)'),
         h3(paste('Quiz 10')),
-#     verbatimTextOutput("StudentName"),
         textOutput('recentQuizScore'),
- #    h4(paste('Added: Boxplot Scores, Top')),
-        plotOutput('histRecentQuiz')
-      #)
-),
-mainPanel(
+        plotOutput('histRecentQuiz'),
+        
+        h2('Grade Modeler'),
+        h4('Use sliders to model Projected Final Grade'),
+        
+        sliderInput("sliderAssignmentsAve", label = h3("Projected Assignments Ave Grade"), 
+                    min = 0, max = 7, value = 4, step = 1),
+                       
+        sliderInput("sliderExam3", label = h3("Projected Exam 3 Grade"), 
+                    min = 0, max = 100, value = 65),
+                       
+        sliderInput("sliderFinal", label = h3("Projected Final Exam Grade"), 
+                    min = 0, max = 100, value = 65),
+        
+        h2('Projected Final Grade'),
+        textOutput('projectedGrade'),
+                       
+        h2('Projected Final Letter Grade'),
+        textOutput('projectedLetterGrade')
+    ), # sidebarPanel
+##########
+    mainPanel(
       wellPanel(
-        h4('Scores Summary'),
+        h2('Scores Summary'),
         tableOutput("tabScores"),
         plotOutput('plotScores'),
-        plotOutput('boxplotScores')
-      )
-),
-      wellPanel(
-        h1('Scores Histograms'),
+        plotOutput('boxplotScores'),
+
+        h2('Scores Histograms'),
         plotOutput('histCurvedScore'),
         plotOutput('histExamAve'),
         plotOutput('histQuiz'),
@@ -60,8 +54,8 @@ mainPanel(
         plotOutput('histOnlineHW'),
         plotOutput('histMATLAB')
       )
-
-    )# conditionalPanel
+    ) # mainPanel
+#  )# conditionalPanel
 
   )# pageWithSidebar
  )# fluidPage
